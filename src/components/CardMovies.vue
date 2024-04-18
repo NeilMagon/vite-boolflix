9be2d28ff5850f082a7ee049254fa0a8f057a0d5
NeilMagon <neil.magon97@gmail.com>
2024-04-18 14:52:49 +0200

    feat: add flag language

--- src/components/CardMovies.vue
+++ src/components/CardMovies.vue
@@ -7,9 +7,20 @@
         },
         data() {
             return {
-                store
+                flags: [
+                    {lang: 'it'},
+                    {lang: 'en'},
+                    {lang: 'fr'},
+                    {lang: 'de'},
+                    {lang: 'es'},
+                ],
             };
         },
+        methods: {
+            getImageUrl(name) {
+                return new URL(`../assets/img/${name}-flag.png`, import.meta.url).href
+            }
+        }
     }
 </script>
 
@@ -17,7 +28,36 @@
     <div class="col">
         <div>Name:{{ cardInfo.title }}</div>
         <div>Original Name: {{ cardInfo.original_title }}</div>
-        <div>Language: {{ cardInfo.original_language }}</div>
+        <div class="img-container" v-if="cardInfo.original_language === flags[0].lang">
+            Language: 
+            <div class="flag-container">
+                <img :src="getImageUrl(flags[0].lang)" :alt="cardInfo.original_language">
+            </div>
+        </div>
+        <div class="img-container" v-else-if="cardInfo.original_language === flags[1].lang">
+            Language: 
+            <div class="flag-container">
+                <img :src="getImageUrl(flags[1].lang)" :alt="cardInfo.original_language">
+            </div>
+        </div>
+        <div class="img-container" v-else-if="cardInfo.original_language === flags[2].lang">
+            Language: 
+            <div class="flag-container">
+                <img :src="getImageUrl(flags[2].lang)" :alt="cardInfo.original_language">
+            </div>
+        </div>
+        <div class="img-container" v-else-if="cardInfo.original_language === flags[3].lang">
+            Language: 
+            <div class="flag-container">
+                <img :src="getImageUrl(flags[3].lang)" :alt="cardInfo.original_language">
+            </div>
+        </div>
+        <div class="img-container" v-else-if="cardInfo.original_language === flags[4].lang">
+            Language: 
+            <div class="flag-container">
+                <img :src="getImageUrl(flags[4].lang)" :alt="cardInfo.original_language">
+            </div>
+        </div>
         <div>Vote:{{ cardInfo.vote_average }}</div>
     </div>
 </template>
@@ -26,5 +66,17 @@
     @use '../style/partials/variables' as *;
     .col{
             width: calc(100% / 4 - 15px);
+            .img-container{
+                width: 200px;
+                display: flex;
+                .flag-container{
+                    width: 30px;
+                }
+            }
+            img{
+                width: 100%;
+            }
         }
+   
+
 </style>
--- src/store.js
+++ src/store.js
@@ -3,4 +3,5 @@ import { reactive } from "vue";
 export const store = reactive ({
     userInput: '',
     movies: [],
+    series: [],
 });
